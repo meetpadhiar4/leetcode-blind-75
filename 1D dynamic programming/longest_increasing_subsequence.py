f276a6c3c6680fc0b48de11711f13feb46173046
Meet Padhiar <67577022+meetpadhiar4@users.noreply.github.com>
2022-09-06 13:47:25 -0500

    Create longest_increasing_subsequence.py

---
+++ 1D dynamic programming/longest_increasing_subsequence.py
@@ -0,0 +1,11 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        dp = [1] * len(nums)
+        max_len = 1
+        for i in range(1, len(nums)):
+            for j in range(i, -1, -1):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+                    max_len = max(max_len, dp[i])
+                   
+        return max_len
